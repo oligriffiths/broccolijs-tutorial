16dc1ffe99a65f0edd60c3b06412d8a122ecb091
Oli Griffiths <oligriffiths@users.noreply.github.com>
2018-03-07 22:05:18 -0500

    03-merge-trees

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,30 @@
 const Funnel = require("broccoli-funnel");
+const Merge = require("broccoli-merge-trees");
 
-module.exports = new Funnel('app', {
+const appRoot = "app";
+
+// Copy HTML file from app root to destination
+const html = new Funnel(appRoot, {
   files: ["index.html"],
   destDir: "/"
 });
+
+// Copy JS file into assets
+const js = new Funnel(appRoot, {
+  files: ["app.js"],
+  destDir: "/assets"
+});
+
+// Copy CSS file into assets
+const css = new Funnel(appRoot, {
+  srcDir: "styles",
+  files: ["app.css"],
+  destDir: "/assets"
+});
+
+// Copy public files into destination
+const public = new Funnel('public', {
+  destDir: "/"
+});
+
+module.exports = new Merge([html, js, css, public]);
